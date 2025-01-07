f17c43a8112bfa0873b7f7dd1282d5fc760af27e
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-07 18:38:55 +0100

    Refactor

--- src/main/scala/wdfeer/avarus/Avarus.scala
+++ src/main/scala/wdfeer/avarus/Avarus.scala
@@ -8,6 +8,8 @@ import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.item.{Item, Items}
 import net.minecraft.server.command.ServerCommandSource
+import net.minecraft.server.network.ServerPlayerEntity
+import net.minecraft.text.Text
 import org.slf4j.LoggerFactory
 
 object Avarus extends ModInitializer {
@@ -26,14 +28,30 @@ object Avarus extends ModInitializer {
     dispatcher.register(builder)
   }
 
-  enum AvarusSubcommand(val item: Item, val action: CommandContext[ServerCommandSource] => Int):
-    case Cobblestone extends AvarusSubcommand(Items.COBBLESTONE, context => {
-      val player = context.getSource.getPlayerOrThrow
+  private val ITEMS_REQUIRED = 512
+
+  enum AvarusSubcommand(val action: CommandContext[ServerCommandSource] => Int):
+    case Cobblestone extends AvarusSubcommand(context => {
+      itemCommand(context, Items.COBBLESTONE, player => {
+        player.addExperience(ITEMS_REQUIRED) // TODO: replace with a permanent stat boost
+      })
+    })
+
+  def itemCommand(context: CommandContext[ServerCommandSource], item: Item, onSuccess: ServerPlayerEntity => Unit): Int = {
+    val player = context.getSource.getPlayerOrThrow
+    val count = player.getInventory.count(Items.COBBLESTONE)
+    if count >= ITEMS_REQUIRED then {
       for
         i <- 0 until (player.getInventory.size - 1)
         if player.getInventory.getStack(i).getItem == Items.COBBLESTONE
-      do player.getInventory.removeStack(i) // TODO: implement intended logic
+      do player.getInventory.removeStack(i)
 
+      onSuccess(player)
       0
-    })
+    }
+    else {
+      context.getSource.sendMessage(Text.of(s"Not enough items! ($count out of $ITEMS_REQUIRED)"))
+      1
+    }
+  }
 }
