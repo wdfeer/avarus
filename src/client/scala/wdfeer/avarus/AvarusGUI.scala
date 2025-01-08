475ac10d8a5b859fa94cff565c60cd7fe4f04d1a
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-08 18:48:58 +0100

    Add owolib dependency

--- src/client/scala/wdfeer/avarus/AvarusGUI.scala
+++ src/client/scala/wdfeer/avarus/AvarusGUI.scala
@@ -1,11 +1,11 @@
 package wdfeer.avarus
 
-import net.fabricmc.api.{EnvType, Environment}
 import net.minecraft.client.gui.screen.Screen
 import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.client.gui.widget.ButtonWidget
 import net.minecraft.text.Text
 
+// TODO: replace with owo-ui
 class AvarusGUI extends Screen(Text.translatable("wdfeer.avarus.gui.title")):
   override def init(): Unit = {
     val button = ButtonWidget.builder(Text.literal("Button 1"), button => {
