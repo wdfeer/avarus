3886094f6e95cb3a1dec3bfefbec789333ed3c46
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-08 16:42:14 +0100

    Create keybind

--- src/client/scala/wdfeer/avarus/AvarusClient.scala
+++ src/client/scala/wdfeer/avarus/AvarusClient.scala
@@ -1,6 +1,24 @@
 package wdfeer.avarus
 
 import net.fabricmc.api.ClientModInitializer
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents
+import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper
+import net.minecraft.client.option.KeyBinding
+import net.minecraft.client.util.InputUtil
+import net.minecraft.text.Text
+import org.lwjgl.glfw.GLFW
 
 object AvarusClient extends ClientModInitializer:
-  override def onInitializeClient(): Unit = ???
+  override def onInitializeClient(): Unit = {
+    val keyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+      "wdfeer.avarus.avarus_open_gui_keybind",
+      InputUtil.Type.KEYSYM,
+      GLFW.GLFW_KEY_SEMICOLON,
+      "wdfeer.avarus.avarus_keybinds"
+    ))
+
+    ClientTickEvents.END_CLIENT_TICK.register(client => {
+      // TODO: show gui screen
+      while (keyBinding.wasPressed) client.player.sendMessage(Text.literal("Key 1 was pressed!"), false)
+    })
+  }
