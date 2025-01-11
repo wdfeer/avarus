70bef304ce89a24e4dee95fabeb8ac63abdd998d
wdfeer <49950781+wdfeer@users.noreply.github.com>
2025-01-11 08:32:33 +0100

    Create movement speed and attack dmg buffs

--- src/main/scala/wdfeer/avarus/Buffs.scala
+++ src/main/scala/wdfeer/avarus/Buffs.scala
@@ -5,6 +5,13 @@ import net.minecraft.entity.attribute.EntityAttributes
 import net.minecraft.item.Items
 
 val buffs: List[AttributeBuff] = List(
+  AttributeBuff(Items.FEATHER, 512, 0.05, Operation.ADDITION, EntityAttributes.GENERIC_MOVEMENT_SPEED),
+  AttributeBuff(Items.WHITE_WOOL, 512, 0.05, Operation.ADDITION, EntityAttributes.GENERIC_MOVEMENT_SPEED),
+
+  AttributeBuff(Items.MAGMA_BLOCK, 512, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_ATTACK_DAMAGE),
+  AttributeBuff(Items.MAGMA_CREAM, 128, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_ATTACK_DAMAGE),
+  AttributeBuff(Items.DIAMOND_SWORD, 32, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_ATTACK_DAMAGE),
+
   AttributeBuff(Items.COBBLESTONE, 2048, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_ARMOR),
   AttributeBuff(Items.COBBLED_DEEPSLATE, 2048, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_ARMOR),
   AttributeBuff(Items.STONE, 2024, 2.0, Operation.ADDITION, EntityAttributes.GENERIC_ARMOR),
@@ -21,10 +28,8 @@ val buffs: List[AttributeBuff] = List(
   AttributeBuff(Items.SUGAR_CANE, 512, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_MAX_HEALTH),
   AttributeBuff(Items.CACTUS, 512, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_MAX_HEALTH),
   AttributeBuff(Items.MUSHROOM_STEW, 32, 1.0, Operation.ADDITION, EntityAttributes.GENERIC_MAX_HEALTH),
-
   AttributeBuff(Items.COOKIE, 1024, 2.0, Operation.ADDITION, EntityAttributes.GENERIC_MAX_HEALTH),
   AttributeBuff(Items.BREAD, 512, 2.0, Operation.ADDITION, EntityAttributes.GENERIC_MAX_HEALTH),
   AttributeBuff(Items.CAKE, 32, 2.0, Operation.ADDITION, EntityAttributes.GENERIC_MAX_HEALTH),
-
-  AttributeBuff(Items.NETHER_STAR, 4, 1.5, Operation.MULTIPLY_TOTAL, EntityAttributes.GENERIC_MAX_HEALTH),
+  AttributeBuff(Items.NETHER_STAR, 4, 0.5, Operation.MULTIPLY_TOTAL, EntityAttributes.GENERIC_MAX_HEALTH),
 )
