98854947bae2eed33fc7831788e54fb3187eb896
wdfeer <49950781+wdfeer@users.noreply.github.com>
2026-06-07 13:18:32 +0200

    Change commands from standalones to subcommands

--- src/main/kotlin/wdfeer/avarus/CommandHelper.kt
+++ src/main/kotlin/wdfeer/avarus/CommandHelper.kt
@@ -1,9 +1,6 @@
 package wdfeer.avarus
 
 import com.mojang.brigadier.Command
-import com.mojang.brigadier.CommandDispatcher
-import com.mojang.brigadier.context.CommandContext
-import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
 import net.minecraft.server.network.ServerPlayerEntity
 import net.minecraft.text.Text
@@ -18,32 +15,30 @@ sealed class CommandResult(val success: Boolean) {
     data class Failure(val error: String) : CommandResult(false)
 }
 
-/** Registers a command that sends a message to the player, always succeeds. */
-fun registerMessageCommand(
-    dispatcher: CommandDispatcher<ServerCommandSource>,
-    name: String,
-    makeMessage: (CommandContext<ServerCommandSource>) -> Text
-) {
-    dispatcher.register(literal(name).executes { context ->
-        context.source.sendMessage(makeMessage(context))
-        0
-    })
+/** Returns a command that sends a message to the player as a success. */
+fun toMessageCommand(
+    makeMessage: (ServerPlayerEntity) -> String
+): Command<ServerCommandSource> {
+    return toCommand {
+        CommandResult.Success(makeMessage(it))
+    }
 }
 
-/** Registers a command that performs an action on the player and sends an error message on error. */
-fun toCommand(execute: (ServerPlayerEntity) -> CommandResult): Command<ServerCommandSource> {
-    return Command { context ->
-        val player = context.source.player
-        if (player != null) {
-            val result = execute(player)
-            when (result) {
-                CommandResult.SilentSuccess -> {}
-                is CommandResult.Success -> context.source.sendMessage(Text.of(result.info))
-                is CommandResult.Failure -> context.source.sendMessage(Text.of(result.error))
-            }
-            result.number
-        } else {
-            getResultNumber(false)
+// TODO: create an extension, to use
+//       `builder.toCommand { ... }` instead of `builder.execute(toCommand { ... })`
+
+/** Returns a command that performs an action on the player and sends an error message on error. */
+fun toCommand(execute: (ServerPlayerEntity) -> CommandResult): Command<ServerCommandSource> = Command { context ->
+    val player = context.source.player
+    if (player != null) {
+        val result = execute(player)
+        when (result) {
+            CommandResult.SilentSuccess -> {}
+            is CommandResult.Success -> context.source.sendMessage(Text.of(result.info))
+            is CommandResult.Failure -> context.source.sendMessage(Text.of(result.error))
         }
+        result.number
+    } else {
+        getResultNumber(false)
     }
 }
--- src/main/kotlin/wdfeer/avarus/Commands.kt
+++ src/main/kotlin/wdfeer/avarus/Commands.kt
@@ -1,111 +1,97 @@
 package wdfeer.avarus
 
+import com.mojang.brigadier.Command
 import com.mojang.brigadier.CommandDispatcher
 import com.mojang.brigadier.builder.LiteralArgumentBuilder
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback
 import net.minecraft.server.command.CommandManager.literal
 import net.minecraft.server.command.ServerCommandSource
-import net.minecraft.text.Text
 import wdfeer.avarus.CommandResult.*
+
+//** Helper class containing state for building the commands. */
+private class AvarusCommander(
+    dispatcher: CommandDispatcher<ServerCommandSource>,
+    private val parentCommand: LiteralArgumentBuilder<ServerCommandSource>,
+    registerSubCommands: AvarusCommander.() -> Unit
+) {
+    init {
+        registerSubCommands()
+        dispatcher.register(parentCommand)
+    }
+
+    fun subcommand(
+        name: String,
+        block: LiteralArgumentBuilder<ServerCommandSource>.() -> Unit
+    ) {
+        parentCommand.then(literal(name).apply(block))
+    }
+
+    fun subcommand(name: String, executes: Command<ServerCommandSource>) {
+        parentCommand.then(literal(name).executes(executes))
+    }
+}
 
 fun initializeCommands(config: Config) {
     val buffs = config.buffs
     CommandRegistrationCallback.EVENT.register { commandDispatcher, _, _ ->
-        registerMessageCommand(commandDispatcher, "avarus-help") {
-            Text.of(
+        AvarusCommander(commandDispatcher, literal("avarus")) {
+            subcommand("help", toMessageCommand {
                 "Avarus is a mod allowing to obtain stat increases (e.g. max hp) by \"buying\" them with a large amount of items with the \"avarus-get\" command, e.g.:\n/avarus cobblestone"
-            )
+            })
+            subcommand("status", toMessageCommand { player ->
+                val (applied, notApplied) = buffs.partition { it.isApplied(player) }
+                buildString {
+                    append("${applied.size}/${buffs.size} buffs applied.\n\n")
+                    append("Available buffs: ")
+                    append(
+                        notApplied.take(3).joinToString(", ") { it.item.toString() })
+                    append(if (notApplied.size <= 3) "" else ", +${notApplied.size - 3}")
+                }
+            })
+            subcommand("get") {
+                for (b in buffs) {
+                    then(
+                        literal(b.name).executes(toCommand { b.tryApply(it) })
+                    )
+                }
+
+                // TODO: make "all" an illegal buff name
+                then(
+                    literal("all").executes(
+                        toCommand { player ->
+                            if (player.isCreative) {
+                                val results = buffs.map { buff -> buff.tryApply(player) }
+                                val successCount = results.count { it.success }
+                                if (successCount != 0) {
+                                    Success("$successCount buffs applied.")
+                                } else Failure("All buffs already applied!")
+                            } else {
+                                Failure("You must be in creative mode!")
+                            }
+                        }
+                    ))
+            }
+            subcommand("remove") {
+                requires { it.hasPermissionLevel(2) }
+
+                for (buff in buffs) {
+                    then(
+                        literal(buff.name).executes(toCommand {
+                            buff.remove(it)
+                            Success("${buff.name} buff removed.")
+                        })
+                    )
+                }
+
+                then(literal("all").executes(toCommand { player ->
+                    var count = 0
+                    buffs.filter { it.isApplied(player) }.forEach {
+                        it.remove(player)
+                        count++
+                    }
+                    Success("$count buffs removed.")
+                }))
+            }
         }
-        registerMessageCommand(commandDispatcher, "avarus-status") { context ->
-            val player = context.source.player ?: return@registerMessageCommand Text.of("No player found!")
-            val (applied, notApplied) = buffs.partition { it.isApplied(player) }
-            Text.of(
-                "${applied.size}/${buffs.size} buffs applied.\n\n" +
-                    "Available buffs: " +
-                    notApplied.take(3).joinToString(", ") { it.item.toString() } +
-                    if (notApplied.size <= 3) "" else ", +${notApplied.size - 3}"
-            )
-        }
-        registerGetCommand(commandDispatcher, buffs)
-        registerGetAllCommand(commandDispatcher, buffs)
-        registerRemoveCommand(commandDispatcher, buffs)
-        registerRemoveAllCommand(commandDispatcher, buffs)
     }
-}
-
-private fun registerGetCommand(
-    dispatcher: CommandDispatcher<ServerCommandSource>,
-    buffs: List<AttributeBuff>
-) {
-    var builder: LiteralArgumentBuilder<ServerCommandSource> = literal("avarus-get")
-
-    for (buff in buffs) {
-        builder = builder.then(
-            literal(buff.name)
-                .executes(toCommand { buff.tryApply(it) })
-        )
-    }
-
-    dispatcher.register(builder)
-}
-
-private fun registerGetAllCommand(
-    dispatcher: CommandDispatcher<ServerCommandSource>,
-    buffs: List<AttributeBuff>
-) {
-    var builder: LiteralArgumentBuilder<ServerCommandSource> = literal("avarus-get-all")
-    builder = builder.requires { it.hasPermissionLevel(2) }
-
-    builder = builder.executes(toCommand { player ->
-        if (player.isCreative) {
-            val results = buffs.map { buff -> buff.tryApply(player) }
-            val successCount = results.count { it.success }
-            if (successCount != 0) {
-                Success("$successCount buffs applied.")
-            } else Failure("All buffs already applied!")
-        } else {
-            Failure("You must be in creative mode!")
-        }
-    })
-
-    dispatcher.register(builder)
-}
-
-private fun registerRemoveCommand(
-    dispatcher: CommandDispatcher<ServerCommandSource>,
-    buffs: List<AttributeBuff>
-) {
-    var builder: LiteralArgumentBuilder<ServerCommandSource> = literal("avarus-remove")
-    builder = builder.requires { it.hasPermissionLevel(2) }
-
-    for (buff in buffs) {
-        builder = builder.then(
-            literal(buff.name)
-                .executes(toCommand {
-                    buff.remove(it)
-                    Success("${buff.name} buff removed.")
-                })
-        )
-    }
-
-    dispatcher.register(builder)
-}
-
-private fun registerRemoveAllCommand(
-    dispatcher: CommandDispatcher<ServerCommandSource>,
-    buffs: List<AttributeBuff>
-) {
-    var builder: LiteralArgumentBuilder<ServerCommandSource> = literal("avarus-remove-all")
-    builder = builder.requires { it.hasPermissionLevel(2) }
-
-    builder = builder.executes(toCommand { player ->
-        var count = 0
-        buffs.filter { it.isApplied(player) }.forEach {
-            it.remove(player)
-            count++
-        }
-        Success("$count buffs removed.")
-    })
-
-    dispatcher.register(builder)
 }
